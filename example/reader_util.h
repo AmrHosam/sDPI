eded8a79c4148d183a3ad8becfeb2b31635e6d9b
omarnagah <omarnagah4@gmail.com>
2020-01-26 14:45:12 +0200

    Fixed directories

--- example/reader_util.h
+++ example/reader_util.h
@@ -1,2 +1,24 @@
 #include <pcap.h>
 #define MAX_NUM_READER_THREADS     16
+#include "ndpi_includes.h"
+
+// workflow main structure
+typedef struct ndpi_workflow {
+  u_int64_t last_time;
+
+  struct ndpi_workflow_prefs prefs;
+  struct ndpi_stats stats;
+
+  ndpi_workflow_callback_ptr __flow_detected_callback;
+  void * __flow_detected_udata;
+  ndpi_workflow_callback_ptr __flow_giveup_callback;
+  void * __flow_giveup_udata;
+
+  /* outside referencies */
+  pcap_t *pcap_handle;
+
+  /* allocated by prefs */
+  void **ndpi_flows_root;
+  struct ndpi_detection_module_struct *ndpi_struct;
+  u_int32_t num_allocated_flows;
+ } ndpi_workflow_t;
