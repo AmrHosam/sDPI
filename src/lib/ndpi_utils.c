beb2a4b384b089651cbb7b75a7011aeb29532ce8
Amr Hossam <amrhossam97@gmail.com>
2020-02-02 10:56:18 +0200

    add twalk

--- src/lib/ndpi_utils.c
+++ src/lib/ndpi_utils.c
@@ -1,3 +1,31 @@
+#ifdef HAVE_CONFIG_H
+#include "ndpi_config.h"
+#endif
+
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include "ahocorasick.h"
+#include "libcache.h"
+
+#define NDPI_CURRENT_PROTO NDPI_PROTOCOL_UNKNOWN
+
+#include "ndpi_api.h"
+#include "ndpi_config.h"
+
+#include <time.h>
+#ifndef WIN32
+#include <unistd.h>
+#endif
+
+#if defined __FreeBSD__ || defined __NetBSD__ || defined __OpenBSD__
+#include <sys/endian.h>
+#endif
+
+#include "third_party/include/ndpi_patricia.h"
+#include "third_party/include/ht_hash.h"
+
+#define NDPI_CONST_GENERIC_PROTOCOL_NAME  "GenericProtocol"
 /* find a node, or return 0 */
 void * ndpi_tfind(const void *vkey, void *vrootp,
 		  int (*compar)(const void *, const void *))
@@ -44,4 +72,92 @@ void * ndpi_tsearch(const void *vkey, void **vrootp,
     q->left = q->right = (ndpi_node *)0;
   }
   return ((void *)q->key);
+}
+
+/* ****************************************** */
+/* Walk the nodes of a tree */
+static void ndpi_tdestroy_recurse(ndpi_node* root, void (*free_action)(void *))
+{
+  if(root->left != NULL)
+    ndpi_tdestroy_recurse(root->left, free_action);
+  if(root->right != NULL)
+    ndpi_tdestroy_recurse(root->right, free_action);
+
+  (*free_action) ((void *) root->key);
+  ndpi_free(root);
+}
+
+/* ****************************************** */
+void ndpi_tdestroy(void *vrootp, void (*freefct)(void *))
+{
+  ndpi_node *root = (ndpi_node *) vrootp;
+
+  if(root != NULL)
+    ndpi_tdestroy_recurse(root, freefct);
+}
+
+/* Walk the nodes of a tree */
+static void ndpi_trecurse(ndpi_node *root, void (*action)(const void *, ndpi_VISIT, int, void*), int level, void *user_data)
+{
+  if(root->left == (ndpi_node *)0 && root->right == (ndpi_node *)0)
+    (*action)(root, ndpi_leaf, level, user_data);
+  else {
+    (*action)(root, ndpi_preorder, level, user_data);
+    if(root->left != (ndpi_node *)0)
+      ndpi_trecurse(root->left, action, level + 1, user_data);
+    (*action)(root, ndpi_postorder, level, user_data);
+    if(root->right != (ndpi_node *)0)
+      ndpi_trecurse(root->right, action, level + 1, user_data);
+    (*action)(root, ndpi_endorder, level, user_data);
+  }
+}
+
+/* Walk the nodes of a tree */
+void ndpi_twalk(const void *vroot, void (*action)(const void *, ndpi_VISIT, int, void *), void *user_data)
+{
+  ndpi_node *root = (ndpi_node *)vroot;
+
+  if(root != (ndpi_node *)0 && action != (void (*)(const void *, ndpi_VISIT, int, void*))0)
+    ndpi_trecurse(root, action, 0, user_data);
+}
+
+/* delete node with given key */
+void * ndpi_tdelete(const void *vkey, void **vrootp, int (*compar)(const void *, const void *))
+{
+  ndpi_node **rootp = (ndpi_node **)vrootp;
+  char *key = (char *)vkey;
+  ndpi_node *q;
+  ndpi_node *r;
+  int cmp;
+
+  if(rootp == (ndpi_node **)0 || *rootp == (ndpi_node *)0)
+    return((void *)0);
+  while ((cmp = (*compar)(key, (*rootp)->key)) != 0) {
+    rootp = (cmp < 0) ?
+      &(*rootp)->left :		/* follow left branch */
+      &(*rootp)->right;		/* follow right branch */
+    if(*rootp == (ndpi_node *)0)
+      return ((void *)0);		/* key not found */
+  }
+  r = (*rootp)->right;			/* D1: */
+  if((q = (*rootp)->left) == (ndpi_node *)0)	/* Left (ndpi_node *)0? */
+    q = r;
+  else if(r != (ndpi_node *)0) {		/* Right link is null? */
+    if(r->left == (ndpi_node *)0) {	/* D2: Find successor */
+      r->left = q;
+      q = r;
+    } else {			/* D3: Find (ndpi_node *)0 link */
+      for(q = r->left; q->left != (ndpi_node *)0; q = r->left)
+	r = q;
+      r->left = q->right;
+      q->left = (*rootp)->left;
+      q->right = (*rootp)->right;
+    }
+  }
+  key = (*rootp)->key;
+  ndpi_free((ndpi_node *) *rootp);	/* D4: Free node */
+  *rootp = q;				/* link parent to new node */
+
+  /* Return the key to give the caller a chance to free custom data */
+  return(key);
 }
