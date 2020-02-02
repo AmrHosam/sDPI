29f01a42571507f1dbd2d085291fa7676fc4733e
omarnagah <omarnagah4@gmail.com>
2020-02-02 11:47:54 +0200

    dont need

--- src/lib/ndpi_utils.c
+++ src/lib/ndpi_utils.c
@@ -1,3 +1,187 @@
+#ifdef HAVE_CONFIG_H
+#include "ndpi_config.h"
+#endif
+
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+//#include "ahocorasick.h"
+//#include "libcache.h"
+
+#define NDPI_CURRENT_PROTO NDPI_PROTOCOL_UNKNOWN
+
+#include "../include/ndpi_api.h"
+//#include "../include/ndpi_config.h"
+
+#include <time.h>
+#ifndef WIN32
+#include <unistd.h>
+#endif
+
+#if defined __FreeBSD__ || defined __NetBSD__ || defined __OpenBSD__
+#include <sys/endian.h>
+#endif
+
+#include "third_party/include/ndpi_patricia.h"
+//#include "third_party/include/ht_hash.h"
+
+#define NDPI_CONST_GENERIC_PROTOCOL_NAME  "GenericProtocol"
+
+/* ***************************************************** */
+/* ******************************************************************** */
+
+static int ndpi_is_other_char(char c) {
+  return((c == '.')
+	 || (c == ' ')
+	 || (c == '@')
+	 || (c == '/')
+	 );
+}
+
+/* ******************************************************************** */
+int ndpi_match_bigram(struct ndpi_detection_module_struct *ndpi_str,
+		      ndpi_automa *automa, char *bigram_to_match) {
+  AC_TEXT_t ac_input_text;
+  AC_REP_t match = { NDPI_PROTOCOL_UNKNOWN, NDPI_PROTOCOL_CATEGORY_UNSPECIFIED, NDPI_PROTOCOL_UNRATED };
+  int rc;
+
+  if((automa->ac_automa == NULL) || (bigram_to_match == NULL))
+    return(-1);
+
+  if(!automa->ac_automa_finalized) {
+    printf("[%s:%d] [NDPI] Internal error: please call ndpi_finalize_initalization()\n", __FILE__, __LINE__);
+    return(0); /* No matches */
+  }
+
+  ac_input_text.astring = bigram_to_match, ac_input_text.length = 2;
+  rc = ac_automata_search(((AC_AUTOMATA_t*)automa->ac_automa), &ac_input_text, &match);
+
+  /*
+    As ac_automata_search can detect partial matches and continue the search process
+    in case rc == 0 (i.e. no match), we need to check if there is a partial match
+    and in this case return it
+  */
+  if((rc == 0) && (match.number != 0)) rc = 1;
+
+  return(rc ? match.number : 0);
+}
+
+/* ****************************************************** */
+/* ******************************************************************** */
+
+static int ndpi_is_valid_char(char c) {
+  if(ispunct(c) && (!ndpi_is_other_char(c)))
+    return(0);
+  else
+    return(isdigit(c)
+	   || isalpha(c)
+	   || ndpi_is_other_char(c));
+}
+
+/* ******************************************************************** */
+
+static int ndpi_find_non_eng_bigrams(struct ndpi_detection_module_struct *ndpi_struct,
+				     char *str) {
+  char s[3];
+
+  if((isdigit(str[0]) && isdigit(str[1]))
+     || ndpi_is_other_char(str[0])
+     || ndpi_is_other_char(str[1])
+     )
+    return(1);
+
+  s[0] = tolower(str[0]), s[1] = tolower(str[1]), s[2] = '\0';
+
+  return(ndpi_match_bigram(ndpi_struct, &ndpi_struct->bigrams_automa, s));
+}
+
+/* ******************************************************************** */
+
+/* ******************************************************************** */
+
+/* #define PRINT_STRINGS 1 */
+
+int ndpi_has_human_readeable_string(struct ndpi_detection_module_struct *ndpi_struct,
+				    char *buffer, u_int buffer_size,
+				    u_int8_t min_string_match_len,
+				    char *outbuf, u_int outbuf_len) {
+  u_int ret = 0, i = 0, do_cr = 0, len = 0, o_idx = 0, being_o_idx = 0;
+
+  if(buffer_size <= 0)
+    return(0);
+
+  outbuf_len--;
+  outbuf[outbuf_len] = '\0';
+
+  for(i=0; i<buffer_size-2; i++) {
+    if(ndpi_is_valid_char(buffer[i])
+       && ndpi_is_valid_char(buffer[i+1])
+       && ndpi_find_non_eng_bigrams(ndpi_struct, &buffer[i])) {
+#ifdef PRINT_STRINGS
+      printf("%c%c", buffer[i], buffer[i+1]);
+#endif
+      if(o_idx < outbuf_len) outbuf[o_idx++] = buffer[i];
+      if(o_idx < outbuf_len) outbuf[o_idx++] = buffer[i+1];
+      do_cr = 1, i += 1, len += 2;
+    } else {
+      if(ndpi_is_valid_char(buffer[i]) && do_cr) {
+#ifdef PRINT_STRINGS
+	printf("%c", buffer[i]);
+#endif
+	if(o_idx < outbuf_len) outbuf[o_idx++] = buffer[i];
+	len += 1;
+      }
+
+      // printf("->> %c%c\n", isprint(buffer[i]) ? buffer[i] : '.', isprint(buffer[i+1]) ? buffer[i+1] : '.');
+      if(do_cr) {
+	if(len > min_string_match_len)
+	  ret = 1;
+	else {
+	  o_idx = being_o_idx;
+	  being_o_idx = o_idx;
+	  outbuf[o_idx] = '\0';
+	}
+
+#ifdef PRINT_STRINGS
+	printf(" [len: %u]%s\n", len, ret ? "<-- HIT" : "");
+#endif
+
+	if(ret)
+	  break;
+
+	do_cr = 0, len = 0;
+      }
+    }
+  }
+
+#ifdef PRINT_STRINGS
+  printf("=======>> Found string: %u\n", ret);
+#endif
+
+  return(ret);
+}
+
+/* ********************************** */
+
+void ndpi_patchIPv6Address(char *str) {
+  int i = 0, j = 0;
+
+  while(str[i] != '\0') {
+    if((str[i] == ':')
+       && (str[i+1] == '0')
+       && (str[i+2] == ':')) {
+      str[j++] = ':';
+      str[j++] = ':';
+      i += 3;
+    } else
+      str[j++] = str[i++];
+  }
+
+  if(str[j] != '\0') str[j] = '\0';
+}
+
+/* ********************************** */
+
 /* find a node, or return 0 */
 void * ndpi_tfind(const void *vkey, void *vrootp,
 		  int (*compar)(const void *, const void *))
